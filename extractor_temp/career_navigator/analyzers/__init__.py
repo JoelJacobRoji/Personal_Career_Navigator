0e0faa24beee0db9edf0da78190c0f5712c0e3af
GitHub Deploy <deploy@github.com>
2026-02-14 14:34:22 +0530

    Cleaned up repository: Removed redundant extractor_temp folder

--- extractor_temp/career_navigator/analyzers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Analyzers package for Career Navigator
-"""
-
-from .job_matcher import JobMatcher
-
-__all__ = ['JobMatcher']
--- extractor_temp/career_navigator/parsers/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Parsers package for Career Navigator
-"""
-
-from .resume_parser import ResumeParser
-from .linkedin_parser import LinkedInParser
-from .github_analyzer import GitHubAnalyzer
-
-__all__ = ['ResumeParser', 'LinkedInParser', 'GitHubAnalyzer']
